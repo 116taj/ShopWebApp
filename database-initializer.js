caa289634f9b84c32b96a3e24ae610361421f28c
Taj Randhawa <116tajisawesome@gmail.com>
2025-07-16 23:43:50 -0400

    changed db initializer to faker
    using branches now as this is WIP

--- database-initializer.js
+++ database-initializer.js
@@ -1,26 +1,33 @@
 
 const mongoose = require("mongoose");
+const { faker } = require('@faker-js/faker');
 const Product = require("./ProductModel");
-
+const User = require("./UserModel");
 const fs = require("fs");
-//db initializer, extract artwork
-//then generates users based on artwork 
+//db initializer, init products and users based on faker
+
 let productArray = [];
-fs.readdir('./products', (err, files) => {
-	console.log(files);
-		if (err)
-		  console.log(err);
-		else {
-			files.forEach(file => {
-				let products = require("./products/" + file);
-				console.log(products.length);
-				products.forEach(product=>{
-				let p = new Product(product);
-				productArray.push(p);
-			});
-		});
-	}
-}); 
+let userArray = [];
+for (i = 0; i < 50; i++){
+	let productName = faker.commerce.product();
+	let productPrice = faker.number.int({min: 1, max: 99});
+	let productStock = faker.number.int({min: 0, max: 500});
+	let productDesc = faker.commerce.productDescription();
+	let productCat = faker.commerce.department();
+	let productImg = "/images/sample"+faker.number.int({min: 1, max: 5})+".png"
+	let product = {"name": productName, "price": productPrice, "stock": productStock, "description": productDesc, "category": productCat, image: productImg};
+	let p = Product(product);
+	productArray.push(p);
+}
+
+for (i = 0; i < 10; i++){
+	let uName = faker.internet.displayName();
+	let uPass = faker.internet.password();
+	let uAdmin = (i == 0)? true : false;
+	let user = {"name": uName, "password": uPass, "isAdmin": uAdmin}
+	let u = User(user)
+	userArray.push(u);
+}
 //create db
 mongoose.connect('mongodb://127.0.0.1/shop', {useNewUrlParser: true, useUnifiedTopology: true});
 let db = mongoose.connection;
@@ -29,13 +36,26 @@ db.once('open', async function() {
 	await mongoose.connection.dropDatabase();
 	console.log("Dropped database. Starting re-creation.");
 	let completedProducts = 0;
-	productArray.forEach(product => {
+	let completedUsers = 0;
+	userArray.forEach(user => {
+		console.log(user)
+		user.save(function(err,result){
+			completedUsers++;
+			if (err) throw err;
+			if (completedUsers >= userArray.length && completedProducts >= productArray.length){
+				console.log("Completed");
+				process.exit();
+			}
+		});
+	});
+	await productArray.forEach(product => {
 		console.log(product)
 		product.save(function(err,result){
 			completedProducts++;
-			if(err) throw err;
-			if (completedProducts >= productArray.length){
+			if (err) throw err;
+			if (completedUsers >= userArray.length && completedProducts >= productArray.length){
 				console.log("Completed");
+				process.exit();
 			}
 
 		});
